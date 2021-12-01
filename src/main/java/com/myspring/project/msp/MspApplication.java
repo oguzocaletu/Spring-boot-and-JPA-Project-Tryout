0547bf8b23261d8506ff1ae084c20df7b189dccf
oguzocaletu <oguzocaletu@gmail.com>
2021-12-01 09:44:36 +0300

    port problem fixed

--- src/main/java/com/myspring/project/msp/MspApplication.java
+++ src/main/java/com/myspring/project/msp/MspApplication.java
@@ -1,14 +1,23 @@
 package com.myspring.project.msp;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Collections;
 
 
 @SpringBootApplication
 public class MspApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MspApplication.class, args);
-	}
+    public static void main(String[] args) {
+
+//		SpringApplication.run(MspApplication.class, args);
+
+        SpringApplication app = new SpringApplication(MspApplication.class);
+        app.setDefaultProperties(Collections
+                .singletonMap("server.port", "8083"));
+        app.run(args);
+    }
 
 
 }
---
+++ src/main/java/com/myspring/project/msp/student/StudentConfig.java
@@ -0,0 +1,46 @@
+package com.myspring.project.msp.student;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+@Configuration
+public class StudentConfig {
+
+    CommandLineRunner commandLineRunner(
+            StudentRepository repository
+    ){
+        return args -> {
+            Student Faruk=new Student(
+                    1L,
+                    "Faruk",
+                    32,
+                    "FarukYilmaz@gmail.com",
+                    LocalDate.of(1989, Month.FEBRUARY,11)
+
+            );
+            Student Fatma= new Student (
+                    2L,
+                    "Fatma",
+                    28,
+                    "Fatma@gmail.com",
+                    LocalDate.of(1993, Month.DECEMBER,21)
+
+            );
+            Student Ayca= new Student (
+                    3L,
+                    "Fatma",
+                    26,
+                    "Fatma@gmail.com",
+                    LocalDate.of(1995, Month.OCTOBER,30)
+
+            );
+            repository.saveAll(
+                    List.of(Faruk,Fatma,Ayca)
+            );
+        };
+    }
+}
